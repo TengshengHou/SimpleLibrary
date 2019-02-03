b9c1c173ecbd00e379c683340a7abbbbc59dd867
TengshengHou <95377774@qq.com>
2019-02-04 06:35:47 +0800

    文件读写处理

--- SimpleLibrary/File/Filehelp.cs
+++ SimpleLibrary/File/Filehelp.cs
@@ -9,14 +9,14 @@ namespace SimpleLibrary.File
     public static class Filehelp
     {
         /// <summary>
-        /// 读取文件内容到字符串
+        /// 异步读取文件内容到字符串（windows磁盘队列支持，大文件优先使用）
         /// </summary>
         /// <param name="path">文件链接</param>
         /// <returns>返回字符串（Utf8）</returns>
-        private static async Task<string> ReadFile2StrAsync(string path)
+        public static async Task<string> ReadFile2StrAsync(string path)
         {
             StringBuilder sb = new StringBuilder(100);
-            const int FILE_READ_SIZE = 1000;
+            const int FILE_READ_SIZE = 4096;
             using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, FILE_READ_SIZE, true))
             {
                 byte[] vs = new byte[0x1000];
@@ -28,6 +28,48 @@ namespace SimpleLibrary.File
                 }
             }
             return sb.ToString();
+        }
+
+
+        public static async Task<int> WriteFileAsync(System.IO.Stream stream, string path)
+        {
+            const int FILE_WRITE_SIZE = 4096;
+            int writeCount = 0;
+            using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write, FILE_WRITE_SIZE, true))
+            {
+                byte[] byteArr = new byte[500];
+                int readCount = 0;
+                while ((readCount = await stream.ReadAsync(byteArr, 0, byteArr.Length)) > 0)
+                {
+                    await fileStream.WriteAsync(byteArr, 0, readCount);
+                    writeCount += readCount;
+                }
+            }
+            return writeCount;
+        }
+
+
+
+
+        /// <summary>
+        /// 创建GUID文件路径，并创建对应路径
+        /// /basePath+/20190204/+guid
+        /// </summary>
+        /// <param name="basePath"></param>
+        /// <returns></returns>
+        public static string GetFilePath(string basePath)
+        {
+            string filePath = string.Empty;
+            string fileName = Guid.NewGuid().ToString("N");
+            filePath = basePath;
+
+            string dirName = DateTime.Now.ToString("yyyyMMdd");
+            filePath = System.IO.Path.Combine(filePath, dirName);
+            if (!System.IO.Directory.Exists(filePath))
+                System.IO.Directory.CreateDirectory(filePath);
+
+            filePath = System.IO.Path.Combine(filePath, fileName);
+            return filePath;
         }
     }
 }
